6283f9d01717ece991f23b8bee6d1bd9ae2572c2
chroc <chroc7@gmail.com>
2016-12-06 16:17:39 +1100

    Game complete v1

--- js/controller.js
+++ js/controller.js
@@ -1,18 +1,17 @@
 var board = new Array([" "," "," "], [" "," "," "], [" "," "," "]);
 
-// var switchPlayer = false;
-
 // player X total moves
 var p1TotalMovs = 0;
-
 // player O total moves
 var p2TotalMovs = 0;
 
+var movesCounter = 0;
 
 function play(player, xPosition, yPosition){
   // set the player move into the board position
   console.log("playing...");
   board[xPosition][yPosition] = player;
+  movesCounter++;
   var currentSquare = getSquare(xPosition, yPosition);
   if (player === "X") {
     p1TotalMovs++;
@@ -63,98 +62,160 @@ function checkPlay(currentSquare, player, xPosition, yPosition) {
 function checkIfWinSq1(player, xPosition, yPosition){
   // 0, 0
   console.log("checking square 1...");
-  if ((board[xPosition][yPosition] === board[1][0] && board[xPosition][yPosition] === board[2][0]) ||
-      (board[xPosition][yPosition] === board[1][1] && board[xPosition][yPosition] === board[2][2]) ||
-      (board[xPosition][yPosition] === board[0][1] && board[xPosition][yPosition] === board[0][2])) {
-    //.. Winner!!
-    console.log("Winner!!");
+  if ((board[xPosition][yPosition] === board[1][0] && board[xPosition][yPosition] === board[2][0])) {
     winner = true;
+    paintSymbolWinner("symbol4");
+    paintSymbolWinner("symbol7");
+  }else if ((board[xPosition][yPosition] === board[1][1] && board[xPosition][yPosition] === board[2][2])) {
+    winner = true;
+    paintSymbolWinner("symbol5");
+    paintSymbolWinner("symbol9");
+  }else if ((board[xPosition][yPosition] === board[0][1] && board[xPosition][yPosition] === board[0][2])) {
+    winner = true;
+    paintSymbolWinner("symbol2");
+    paintSymbolWinner("symbol3");
   }
+  checkIfDraw();
 }
 
 function checkIfWinSq2(player, xPosition, yPosition){
   // 0, 1
   console.log("checking square 2...");
-  if ((board[xPosition][yPosition] === board[1][1] && board[xPosition][yPosition] === board[2][1]) ||
-      (board[xPosition][yPosition] === board[0][0] && board[xPosition][yPosition] === board[0][2])) {
-    console.log("Winner!!");
+  if ((board[xPosition][yPosition] === board[1][1] && board[xPosition][yPosition] === board[2][1])) {
     winner = true;
+    paintSymbolWinner("symbol5");
+    paintSymbolWinner("symbol8");
+  }else if ((board[xPosition][yPosition] === board[0][0] && board[xPosition][yPosition] === board[0][2])) {
+    winner = true;
+    paintSymbolWinner("symbol1");
+    paintSymbolWinner("symbol3");
   }
+  checkIfDraw();
 }
 
 function checkIfWinSq3(player, xPosition, yPosition){
   // 0, 2
   console.log("checking square 3...");
-  if ((board[xPosition][yPosition] === board[0][0] && board[xPosition][yPosition] === board[0][1]) ||
-      (board[xPosition][yPosition] === board[2][2] && board[xPosition][yPosition] === board[1][2]) ||
-      (board[xPosition][yPosition] === board[2][0] && board[xPosition][yPosition] === board[1][1])) {
-    console.log("Winner!!");
+  if ((board[xPosition][yPosition] === board[0][0] && board[xPosition][yPosition] === board[0][1])) {
     winner = true;
+    paintSymbolWinner("symbol1");
+    paintSymbolWinner("symbol2");
+  }else if ((board[xPosition][yPosition] === board[2][2] && board[xPosition][yPosition] === board[1][2])) {
+    winner = true;
+    paintSymbolWinner("symbol9");
+    paintSymbolWinner("symbol6");
+  }else if ((board[xPosition][yPosition] === board[2][0] && board[xPosition][yPosition] === board[1][1])) {
+    winner = true;
+    paintSymbolWinner("symbol7");
+    paintSymbolWinner("symbol5");
   }
+  checkIfDraw();
 }
 
 function checkIfWinSq4(player, xPosition, yPosition){
   // 1, 0
   console.log("checking square 4...");
-  if ((board[xPosition][yPosition] === board[1][1] && board[xPosition][yPosition] === board[1][2]) ||
-      (board[xPosition][yPosition] === board[0][0] && board[xPosition][yPosition] === board[2][0])) {
-    console.log("Winner!!");
+  if ((board[xPosition][yPosition] === board[1][1] && board[xPosition][yPosition] === board[1][2])) {
     winner = true;
+    paintSymbolWinner("symbol5");
+    paintSymbolWinner("symbol6");
+  }else if ((board[xPosition][yPosition] === board[0][0] && board[xPosition][yPosition] === board[2][0])) {
+    winner = true;
+    paintSymbolWinner("symbol1");
+    paintSymbolWinner("symbol7");
   }
+  checkIfDraw();
 }
 
 function checkIfWinSq5(player, xPosition, yPosition){
   // 1, 1
   console.log("checking square 5...");
-  if ((board[xPosition][yPosition] === board[1][0] && board[xPosition][yPosition] === board[1][2]) ||
-      (board[xPosition][yPosition] === board[0][1] && board[xPosition][yPosition] === board[2][1]) ||
-      (board[xPosition][yPosition] === board[0][0] && board[xPosition][yPosition] === board[2][2]) ||
-      (board[xPosition][yPosition] === board[2][0] && board[xPosition][yPosition] === board[0][2])) {
-    console.log("Winner!!");
+  if ((board[xPosition][yPosition] === board[1][0] && board[xPosition][yPosition] === board[1][2])) {
     winner = true;
+    paintSymbolWinner("symbol4");
+    paintSymbolWinner("symbol6");
+  }else if ((board[xPosition][yPosition] === board[0][1] && board[xPosition][yPosition] === board[2][1])) {
+    winner = true;
+    paintSymbolWinner("symbol2");
+    paintSymbolWinner("symbol8");
+  }else if ((board[xPosition][yPosition] === board[0][0] && board[xPosition][yPosition] === board[2][2])) {
+    winner = true;
+    paintSymbolWinner("symbol1");
+    paintSymbolWinner("symbol9");
+  }else if ((board[xPosition][yPosition] === board[2][0] && board[xPosition][yPosition] === board[0][2])) {
+    winner = true;
+    paintSymbolWinner("symbol7");
+    paintSymbolWinner("symbol3");
   }
+  checkIfDraw();
 }
 
 function checkIfWinSq6(player, xPosition, yPosition){
   // 1, 2
   console.log("checking square 6...");
-  if ((board[xPosition][yPosition] === board[1][0] && board[xPosition][yPosition] === board[1][1]) ||
-      (board[xPosition][yPosition] === board[2][2] && board[xPosition][yPosition] === board[0][2])) {
-    console.log("Winner!!");
+  if ((board[xPosition][yPosition] === board[1][0] && board[xPosition][yPosition] === board[1][1])) {
     winner = true;
+    paintSymbolWinner("symbol4");
+    paintSymbolWinner("symbol5");
+  }else if ((board[xPosition][yPosition] === board[2][2] && board[xPosition][yPosition] === board[0][2])) {
+    winner = true;
+    paintSymbolWinner("symbol9");
+    paintSymbolWinner("symbol3");
   }
+  checkIfDraw();
 }
 
 function checkIfWinSq7(player, xPosition, yPosition){
   // 2, 0
   console.log("checking square 7...");
-  if ((board[xPosition][yPosition] === board[2][2] && board[xPosition][yPosition] === board[2][1]) ||
-      (board[xPosition][yPosition] === board[0][0] && board[xPosition][yPosition] === board[1][0]) ||
-      (board[xPosition][yPosition] === board[1][1] && board[xPosition][yPosition] === board[0][2])) {
-    console.log("Winner!!");
+  if ((board[xPosition][yPosition] === board[2][2] && board[xPosition][yPosition] === board[2][1])) {
     winner = true;
+    paintSymbolWinner("symbol9");
+    paintSymbolWinner("symbol8");
+  }else if ((board[xPosition][yPosition] === board[0][0] && board[xPosition][yPosition] === board[1][0])) {
+    winner = true;
+    paintSymbolWinner("symbol1");
+    paintSymbolWinner("symbol4");
+  }else if ((board[xPosition][yPosition] === board[1][1] && board[xPosition][yPosition] === board[0][2])) {
+    winner = true;
+    paintSymbolWinner("symbol5");
+    paintSymbolWinner("symbol3");
   }
+  checkIfDraw();
 }
 
 function checkIfWinSq8(player, xPosition, yPosition){
   // 2, 1
   console.log("checking square 8...");
-  if ((board[xPosition][yPosition] === board[2][0] && board[xPosition][yPosition] === board[2][2]) ||
-      (board[xPosition][yPosition] === board[1][1] && board[xPosition][yPosition] === board[0][1])) {
-    console.log("Winner!!");
+  if ((board[xPosition][yPosition] === board[2][0] && board[xPosition][yPosition] === board[2][2])) {
     winner = true;
+    paintSymbolWinner("symbol7");
+    paintSymbolWinner("symbol9");
+  }else if ((board[xPosition][yPosition] === board[1][1] && board[xPosition][yPosition] === board[0][1])) {
+    winner = true;
+    paintSymbolWinner("symbol5");
+    paintSymbolWinner("symbol2");
   }
+  checkIfDraw();
 }
 
 function checkIfWinSq9(player, xPosition, yPosition){
   // 2, 2
   console.log("checking square 9...");
-  if ((board[xPosition][yPosition] === board[2][0] && board[xPosition][yPosition] === board[2][1]) ||
-      (board[xPosition][yPosition] === board[0][0] && board[xPosition][yPosition] === board[1][1]) ||
-      (board[xPosition][yPosition] === board[1][2] && board[xPosition][yPosition] === board[0][2])) {
-    console.log("Winner!!");
+  if ((board[xPosition][yPosition] === board[2][0] && board[xPosition][yPosition] === board[2][1])) {
     winner = true;
+    paintSymbolWinner("symbol7");
+    paintSymbolWinner("symbol8");
+  }else if ((board[xPosition][yPosition] === board[0][0] && board[xPosition][yPosition] === board[1][1])) {
+    winner = true;
+    paintSymbolWinner("symbol1");
+    paintSymbolWinner("symbol5");
+  }else if ((board[xPosition][yPosition] === board[1][2] && board[xPosition][yPosition] === board[0][2])) {
+    winner = true;
+    paintSymbolWinner("symbol6");
+    paintSymbolWinner("symbol3");
   }
+  checkIfDraw();
 }
 
 function getSquare(xPosition, yPosition){
--- js/view.js
+++ js/view.js
@@ -1,3 +1,5 @@
+var boardDOMElement = document.getElementById("board");
+
 var square1DOMElement = document.getElementById("square1");
 var square2DOMElement = document.getElementById("square2");
 var square3DOMElement = document.getElementById("square3");
@@ -7,259 +9,364 @@ var square6DOMElement = document.getElementById("square6");
 var square7DOMElement = document.getElementById("square7");
 var square8DOMElement = document.getElementById("square8");
 var square9DOMElement = document.getElementById("square9");
+
 var winTextDOMElement = document.getElementById("win-text");
+var drawTextDOMElement = document.getElementById("draw-text");
+var resetDOMElement = document.getElementById("reset-game");
+// var symbol1DOMElement = document.getElementById("symbol1");
+// var symbol2DOMElement = document.getElementById("symbol2");
+// var symbol3DOMElement = document.getElementById("symbol3");
+// var symbol4DOMElement = document.getElementById("symbol4");
+// var symbol5DOMElement = document.getElementById("symbol5");
+// var symbol6DOMElement = document.getElementById("symbol6");
+// var symbol7DOMElement = document.getElementById("symbol7");
+// var symbol8DOMElement = document.getElementById("symbol8");
+// var symbol9DOMElement = document.getElementById("symbol9");
+var symbol1 = document.createElement("h1");
+var symbol2 = document.createElement("h1");
+var symbol3 = document.createElement("h1");
+var symbol4 = document.createElement("h1");
+var symbol5 = document.createElement("h1");
+var symbol6 = document.createElement("h1");
+var symbol7 = document.createElement("h1");
+var symbol8 = document.createElement("h1");
+var symbol9 = document.createElement("h1");
+
+var currentPlayerDOMElement = document.getElementById("current-player");
+
+var xScoreDOMElement = document.getElementById("x-score");
+var oScoreDOMElement = document.getElementById("o-score");
+var xScoreCounter = 0;
+var oScoreCounter = 0;
+var xScoreNumber = document.createElement("span");
+var oScoreNumber = document.createElement("span");
+
+var winColor = "#CC6600";
 
 var switchPlayer = false;
 var winner = false;
 
+var currentPlayerText = document.createElement("h3");
+
+window.onload = function(){
+  // current player
+  currentPlayerText.textContent = "X";
+  currentPlayerDOMElement.appendChild(currentPlayerText);
+  // score
+  xScoreNumber.textContent = xScoreCounter;
+  oScoreNumber.textContent = oScoreCounter;
+  xScoreDOMElement.appendChild(xScoreNumber);
+  oScoreDOMElement.appendChild(oScoreNumber);
+}
+
 square1DOMElement.addEventListener("click", function(){
+  // debugger;
+  console.log("SQ 1 clicked...");
+  symbol1 = document.createElement("h1");
+  symbol1.setAttribute("id", "symbol1");
   var xPosition = 0;
   var yPosition = 0;
-  var symbol = document.createElement("h1");
   if (switchPlayer === false) {
     var player = "X";
-    symbol.textContent = player;
+    displayCurrentPlayer("O");
     play(player, xPosition, yPosition);
+    isWinner(winner, player, xPosition, yPosition, square1DOMElement, symbol1);
     switchPlayer = true;
-    square1DOMElement.appendChild(symbol);
-    square1DOMElement.disabled = true;
-    square1DOMElement.style.pointerEvents = 'none';
   }else {
     var player = "O";
-    symbol.textContent = player;
+    displayCurrentPlayer("X");
     play(player, xPosition, yPosition);
+    isWinner(winner, player, xPosition, yPosition, square1DOMElement, symbol1);
     switchPlayer = false;
-    square1DOMElement.appendChild(symbol);
-    square1DOMElement.style.pointerEvents = 'none';
-  }
-  // if there is a winner, display text
-  if (winner === true) {
-    var winText = document.createElement("h1");
-    winText.textContent = "Player " + player + " Wins!!";
-    winTextDOMElement.appendChild(winText);
   }
 })
 
 square2DOMElement.addEventListener("click", function(){
+  symbol2 = document.createElement("h1");
+  symbol2.setAttribute("id", "symbol2");
   var xPosition = 0;
   var yPosition = 1;
-  var symbol = document.createElement("h1");
   if (switchPlayer === false) {
     var player = "X";
-    symbol.textContent = player;
-    // call function play
+    displayCurrentPlayer("O");
     play(player, xPosition, yPosition);
+    isWinner(winner, player, xPosition, yPosition, square2DOMElement, symbol2);
     switchPlayer = true;
-    square2DOMElement.appendChild(symbol);
-    square2DOMElement.style.pointerEvents = 'none';
   }else {
     var player = "O";
-    symbol.textContent = player;
+    displayCurrentPlayer("X");
     play(player, xPosition, yPosition);
+    isWinner(winner, player, xPosition, yPosition, square2DOMElement, symbol2);
     switchPlayer = false;
-    square2DOMElement.appendChild(symbol);
-    square2DOMElement.style.pointerEvents = 'none';
-  }
-  // if there is a winner, display text
-  if (winner === true) {
-    var winText = document.createElement("h1");
-    winText.textContent = "Player " + player + " Wins!!";
-    winTextDOMElement.appendChild(winText);
   }
 })
 
 square3DOMElement.addEventListener("click", function(){
+  symbol3 = document.createElement("h1");
+  symbol3.setAttribute("id", "symbol3");
   var xPosition = 0;
   var yPosition = 2;
-  var symbol = document.createElement("h1");
   if (switchPlayer === false) {
     var player = "X";
-    symbol.textContent = player;
-    // call function play
+    displayCurrentPlayer("O");
     play(player, xPosition, yPosition);
+    isWinner(winner, player, xPosition, yPosition, square3DOMElement, symbol3);
     switchPlayer = true;
-    square3DOMElement.appendChild(symbol);
-    square3DOMElement.style.pointerEvents = 'none';
   }else {
     var player = "O";
-    symbol.textContent = player;
+    displayCurrentPlayer("X");
     play(player, xPosition, yPosition);
+    isWinner(winner, player, xPosition, yPosition, square3DOMElement, symbol3);
     switchPlayer = false;
-    square3DOMElement.appendChild(symbol);
-    square3DOMElement.style.pointerEvents = 'none';
-  }
-  // if there is a winner, display text
-  if (winner === true) {
-    var winText = document.createElement("h1");
-    winText.textContent = "Player " + player + " Wins!!";
-    winTextDOMElement.appendChild(winText);
   }
 })
 
 square4DOMElement.addEventListener("click", function(){
+  symbol4 = document.createElement("h1");
+  symbol4.setAttribute("id", "symbol4");
   var xPosition = 1;
   var yPosition = 0;
-  var symbol = document.createElement("h1");
   if (switchPlayer === false) {
     var player = "X";
-    symbol.textContent = player;
-    // call function play
+    displayCurrentPlayer("O");
     play(player, xPosition, yPosition);
+    isWinner(winner, player, xPosition, yPosition, square4DOMElement, symbol4);
     switchPlayer = true;
-    square4DOMElement.appendChild(symbol);
-    square4DOMElement.style.pointerEvents = 'none';
   }else {
     var player = "O";
-    symbol.textContent = player;
+    displayCurrentPlayer("X");
     play(player, xPosition, yPosition);
+    isWinner(winner, player, xPosition, yPosition, square4DOMElement, symbol4);
     switchPlayer = false;
-    square4DOMElement.appendChild(symbol);
-    square4DOMElement.style.pointerEvents = 'none';
-  }
-  // if there is a winner, display text
-  if (winner === true) {
-    var winText = document.createElement("h1");
-    winText.textContent = "Player " + player + " Wins!!";
-    winTextDOMElement.appendChild(winText);
   }
 })
 
 square5DOMElement.addEventListener("click", function(){
+  symbol5 = document.createElement("h1");
+  symbol5.setAttribute("id", "symbol5");
   var xPosition = 1;
   var yPosition = 1;
-  var symbol = document.createElement("h1");
   if (switchPlayer === false) {
     var player = "X";
-    symbol.textContent = player;
-    // call function play
+    displayCurrentPlayer("O");
     play(player, xPosition, yPosition);
+    isWinner(winner, player, xPosition, yPosition, square5DOMElement, symbol5);
     switchPlayer = true;
-    square5DOMElement.appendChild(symbol);
-    square5DOMElement.style.pointerEvents = 'none';
   }else {
     var player = "O";
-    symbol.textContent = player;
+    displayCurrentPlayer("X");
     play(player, xPosition, yPosition);
+    isWinner(winner, player, xPosition, yPosition, square5DOMElement, symbol5);
     switchPlayer = false;
-    square5DOMElement.appendChild(symbol);
-    square5DOMElement.style.pointerEvents = 'none';
-  }
-  // if there is a winner, display text
-  if (winner === true) {
-    var winText = document.createElement("h1");
-    winText.textContent = "Player " + player + " Wins!!";
-    winTextDOMElement.appendChild(winText);
   }
 })
 
 square6DOMElement.addEventListener("click", function(){
+  symbol6 = document.createElement("h1");
+  symbol6.setAttribute("id", "symbol6");
   var xPosition = 1;
   var yPosition = 2;
-  var symbol = document.createElement("h1");
   if (switchPlayer === false) {
     var player = "X";
-    symbol.textContent = player;
-    // call function play
+    displayCurrentPlayer("O");
     play(player, xPosition, yPosition);
+    isWinner(winner, player, xPosition, yPosition, square6DOMElement, symbol6);
     switchPlayer = true;
-    square6DOMElement.appendChild(symbol);
-    square6DOMElement.style.pointerEvents = 'none';
   }else {
     var player = "O";
-    symbol.textContent = player;
+    displayCurrentPlayer("X");
     play(player, xPosition, yPosition);
+    isWinner(winner, player, xPosition, yPosition, square6DOMElement, symbol6);
     switchPlayer = false;
-    square6DOMElement.appendChild(symbol);
-    square6DOMElement.style.pointerEvents = 'none';
-  }
-  // if there is a winner, display text
-  if (winner === true) {
-    var winText = document.createElement("h1");
-    winText.textContent = "Player " + player + " Wins!!";
-    winTextDOMElement.appendChild(winText);
   }
 })
 
 square7DOMElement.addEventListener("click", function(){
+  symbol7 = document.createElement("h1");
+  symbol7.setAttribute("id", "symbol7");
   var xPosition = 2;
   var yPosition = 0;
-  var symbol = document.createElement("h1");
   if (switchPlayer === false) {
     var player = "X";
-    symbol.textContent = player;
-    // call function play
+    displayCurrentPlayer("O");
     play(player, xPosition, yPosition);
+    isWinner(winner, player, xPosition, yPosition, square7DOMElement, symbol7);
     switchPlayer = true;
-    square7DOMElement.appendChild(symbol);
-    square7DOMElement.style.pointerEvents = 'none';
   }else {
     var player = "O";
-    symbol.textContent = player;
+    displayCurrentPlayer("X");
     play(player, xPosition, yPosition);
+    isWinner(winner, player, xPosition, yPosition, square7DOMElement, symbol7);
     switchPlayer = false;
-    square7DOMElement.appendChild(symbol);
-    square7DOMElement.style.pointerEvents = 'none';
-  }
-  // if there is a winner, display text
-  if (winner === true) {
-    var winText = document.createElement("h1");
-    winText.textContent = "Player " + player + " Wins!!";
-    winTextDOMElement.appendChild(winText);
   }
 })
 
 square8DOMElement.addEventListener("click", function(){
+  symbol8 = document.createElement("h1");
+  symbol8.setAttribute("id", "symbol8");
   var xPosition = 2;
   var yPosition = 1;
-  var symbol = document.createElement("h1");
   if (switchPlayer === false) {
     var player = "X";
-    symbol.textContent = player;
-    // call function play
+    displayCurrentPlayer("O");
     play(player, xPosition, yPosition);
+    isWinner(winner, player, xPosition, yPosition, square8DOMElement, symbol8);
     switchPlayer = true;
-    square8DOMElement.appendChild(symbol);
-    square8DOMElement.style.pointerEvents = 'none';
   }else {
     var player = "O";
-    symbol.textContent = player;
+    displayCurrentPlayer("X");
     play(player, xPosition, yPosition);
+    isWinner(winner, player, xPosition, yPosition, square8DOMElement, symbol8);
     switchPlayer = false;
-    square8DOMElement.appendChild(symbol);
-    square8DOMElement.style.pointerEvents = 'none';
-  }
-  // if there is a winner, display text
-  if (winner === true) {
-    var winText = document.createElement("h1");
-    winText.textContent = "Player " + player + " Wins!!";
-    winTextDOMElement.appendChild(winText);
   }
 })
 
 square9DOMElement.addEventListener("click", function(){
+  symbol9 = document.createElement("h1");
+  symbol9.setAttribute("id", "symbol9");
   var xPosition = 2;
   var yPosition = 2;
-  var symbol = document.createElement("h1");
   if (switchPlayer === false) {
     var player = "X";
-    symbol.textContent = player;
-    // call function play
+    displayCurrentPlayer("O");
     play(player, xPosition, yPosition);
+    isWinner(winner, player, xPosition, yPosition, square9DOMElement, symbol9);
     switchPlayer = true;
-    square9DOMElement.appendChild(symbol);
-    square9DOMElement.style.pointerEvents = 'none';
   }else {
     var player = "O";
-    symbol.textContent = player;
+    displayCurrentPlayer("X");
     play(player, xPosition, yPosition);
+    isWinner(winner, player, xPosition, yPosition, square9DOMElement, symbol9);
     switchPlayer = false;
-    square9DOMElement.appendChild(symbol);
-    square9DOMElement.style.pointerEvents = 'none';
-  }
-  // if there is a winner, display text
-  if (winner === true) {
-    var winText = document.createElement("h1");
-    winText.textContent = "Player " + player + " Wins!!";
-    winTextDOMElement.appendChild(winText);
   }
 })
+
+
+function isWinner(winner, player, xPosition, yPosition, squareDOMElement, symbol){
+  if (winner === true) {
+    // change the symbol colorsymbol.textContent = player;
+    symbol.textContent = player;
+    symbol.style.color = winColor;
+    squareDOMElement.appendChild(symbol);
+    squareDOMElement.style.pointerEvents = 'none';
+    // set winner text
+    var winText = document.createElement("h1");
+    winText.textContent = "Player -" + player + "- Wins!!";
+    winText.style.color = winColor;
+    var symbolWin = board[xPosition][yPosition];
+    winTextDOMElement.appendChild(winText);
+    boardDOMElement.style.pointerEvents = 'none';
+    displayScore(player);
+  }else {
+    symbol.textContent = player;
+    squareDOMElement.appendChild(symbol);
+    squareDOMElement.style.pointerEvents = 'none';
+  }
+}
+
+function paintSymbolWinner (symbol){
+  switch (symbol) {
+    case "symbol1":
+      symbol1.style.color = winColor;
+      break;
+    case "symbol2":
+      symbol2.style.color = winColor;
+      break;
+    case "symbol3":
+      symbol3.style.color = winColor;
+      break;
+    case "symbol4":
+      symbol4.style.color = winColor;
+      break;
+    case "symbol5":
+      symbol5.style.color = winColor;
+      break;
+    case "symbol6":
+      symbol6.style.color = winColor;
+      break;
+    case "symbol7":
+      symbol7.style.color = winColor;
+      break;
+    case "symbol8":
+      symbol8.style.color = winColor;
+      break;
+    case "symbol9":
+      symbol9.style.color = winColor;
+      break;
+  }
+}
+
+function checkIfDraw(){
+  if (movesCounter === 9 && winner === false) {
+    var drawText = document.createElement("h1");
+    drawText.textContent = "Draw!";
+    drawText.style.color = winColor;
+    drawTextDOMElement.appendChild(drawText);
+  }
+}
+
+function displayCurrentPlayer(player){
+  currentPlayerDOMElement.removeChild(currentPlayerText);
+  currentPlayerText = document.createElement("h3");
+  currentPlayerText.textContent = player;
+  currentPlayerDOMElement.appendChild(currentPlayerText);
+}
+
+function displayScore(player){
+  if (player === "X") {
+    xScoreCounter++;
+    xScoreDOMElement.removeChild(xScoreNumber);
+    xScoreNumber = document.createElement("span");
+    xScoreNumber.textContent = xScoreCounter;
+    xScoreDOMElement.appendChild(xScoreNumber);
+  }else if (player === "O") {
+    oScoreCounter++;
+    oScoreDOMElement.removeChild(oScoreNumber);
+    oScoreNumber = document.createElement("span");
+    oScoreNumber.textContent = oScoreCounter;
+    oScoreDOMElement.appendChild(oScoreNumber);
+  }
+}
+
+// Button reset game
+resetDOMElement.addEventListener("click", function(){
+  resetGame();
+})
+
+function resetGame(){
+  console.log("Reseting...");
+  // debugger;
+  // square1DOMElement.removeChild(symbol1);
+  square1DOMElement.innerHTML = "";
+  square2DOMElement.innerHTML = "";
+  square3DOMElement.innerHTML = "";
+  square4DOMElement.innerHTML = "";
+  square5DOMElement.innerHTML = "";
+  square6DOMElement.innerHTML = "";
+  square7DOMElement.innerHTML = "";
+  square8DOMElement.innerHTML = "";
+  square9DOMElement.innerHTML = "";
+  winTextDOMElement.innerHTML = "";
+  drawTextDOMElement.innerHTML = "";
+  // location.reload();
+
+  // Enable pointerEvents
+  square1DOMElement.style.pointerEvents = "all";
+  square2DOMElement.style.pointerEvents = "all";
+  square3DOMElement.style.pointerEvents = "all";
+  square4DOMElement.style.pointerEvents = "all";
+  square5DOMElement.style.pointerEvents = "all";
+  square6DOMElement.style.pointerEvents = "all";
+  square7DOMElement.style.pointerEvents = "all";
+  square8DOMElement.style.pointerEvents = "all";
+  square9DOMElement.style.pointerEvents = "all";
+  boardDOMElement.style.pointerEvents = "all";
+  boardDOMElement
+
+  board = new Array([" "," "," "], [" "," "," "], [" "," "," "]);
+  p1TotalMovs = 0;
+  p2TotalMovs = 0;
+  movesCounter = 0;
+  switchPlayer = false;
+  winner = false;
+}
